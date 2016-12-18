01ba00032a66da8edf7e1c101cae6c47370daf4b
Anatoly Chernow <chertoly@gmail.com>
2016-12-18 23:06:39 +0200

    Refactor auto_spec a bit

--- spec/auto_spec.rb
+++ spec/auto_spec.rb
@@ -1,31 +1,20 @@
 require 'helper'
 
 describe 'passing and failing YAML specifications' do
-  passing = %w[
-    0
-    counter
-    echo
-  ]
+  Dir["spec/yml/**/*.yml"].each do |path|
+    name = path.split('/').last
+    test = Hobby::Test.new path
 
-  failing = %w[
-    0
-  ]
-
-  passing.each do |name|
-    path = "spec/yml/passing/#{name}.yml"
-    it "passing #{name}" do
-      test = Hobby::Test.new path
-      report = test[@socket]
-      assert { report.ok? }
-    end
-  end
-
-  failing.each do |name|
-    path = "spec/yml/failing/#{name}.yml"
-    it "failing #{name}" do
-      test = Hobby::Test.new path
-      report = test[@socket]
-      assert { not report.ok? }
+    if path.include? 'passing'
+      it "passing #{name}" do
+        report = test[@socket]
+        assert { report.ok? }
+      end
+    else
+      it "failing #{name}" do
+        report = test[@socket]
+        assert { not report.ok? }
+      end
     end
   end
 end
