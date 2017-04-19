a85606c715d12f13d3d0cfa8f671447b97a7f146
Anatoly Chernow <chertoly@gmail.com>
2017-04-19 23:41:02 +0300

    Start Puma server only for integration specs

--- spec/helper.rb
+++ spec/helper.rb
@@ -8,22 +8,6 @@ require 'puma'
 RSpec.configure do |config|
   config.expect_with :rspec, :minitest
 
-  config.before :each do |example|
-    @socket = "MainApp.for.#{example}.socket"
-    @pid = fork do
-      server = Puma::Server.new MainApp.new
-      server.add_unix_listener @socket
-      server.run
-      sleep
-    end
-
-    sleep 0.01 until File.exist? @socket
-  end
-
-  config.after :each do 
-    `kill #{@pid}`
-  end
-
   config.after :suite do
     `rm *.socket` unless Dir['*.socket'].empty?
   end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,6 +1,22 @@
 require 'helper'
 
 describe 'passing and failing YAML specifications' do
+  before :each do |example|
+    @socket = "MainApp.for.#{example}.socket"
+    @pid = fork do
+      server = Puma::Server.new MainApp.new
+      server.add_unix_listener @socket
+      server.run
+      sleep
+    end
+
+    sleep 0.01 until File.exist? @socket
+  end
+
+  after :each do 
+    `kill #{@pid}`
+  end
+
   Dir["spec/yml/passed/json/*.yml"].each do |path|
     name = path.split('/').last
     test = Hobby::Test.from_file path, format: JSON
